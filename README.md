1c6df93da53888b2fe312aaf604c8c5799df3e03
Paulo Oliveira <pco@dmz.org.br>
2020-07-21 12:29:22 -0300

    Adding comments and readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # computer-vision-color-segmentation
  Color segmentation using OpenCV
+
+To use the color segmentation algorithm, please follow the following steps:
+
+1. Install Python3
+2. Clone the project
+3. Install libs running the command: pip install -r requirements.txt
+4. Access project folder
+5. Run the color_segmentation.py file using command: python color_segmentation.py
--- color_segmentation.py
+++ color_segmentation.py
@@ -11,11 +11,14 @@ upper_range = np.array([10,10,255])
 #create a mask using ranges
 mask = cv2.inRange(image, lower_range, upper_range)
 
+#segment mask area in the original image
 result = cv2.bitwise_and(image, image, mask=mask)
 
+#show original image, mask and segmented image
 cv2.imshow("image", image)
 cv2.imshow("mask", mask)
 cv2.imshow("result", result)
 
+#end of script (wait a key press and close all windows)
 cv2.waitKey(0)
 cv2.destroyAllWindows
